1431e5502164d554bd02f7dabe5c36505b28e33d
noelia96-96 <noeliagalindo96@gmail.com>
2020-11-10 22:05:05 +0100

    commitdefinitivo

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 // En la primera lista tendréis que hacer lo siguiente:
 //    * Añadid la clase "element-n" a cada "span" de la lista, siendo "n" el número del lugar que ocupa en la lista (por ejemplo el segundo "span" de la lista tendría la clase "element-2"). Para ello, haced uso de los selectores de nodo (parentNode, nextSibling, firstChild...) partiendo del "span" con la clase "selected" siempre.
+
 //    * Tras añadir las clases, haced uso de "querySelectorAll" para obtener solo los elementos "li" con valor par (teniendo en cuenta que el primer elmento es el 1) y, a continuación, eliminadlos.
 
 //En la segunda lista, que en principio está vacía, deberéis hacer esto:
@@ -11,6 +12,73 @@
 
 window.addEventListener("load", onLoad);
 
+
+function firstList(){
+  let spanSelected = document.querySelector('.selected');
+  let parentUl = spanSelected.parentNode.parentNode;
+
+  let listLi = parentUl.children;
+
+  let firstChild = parentUl.firstElementChild;
+
+  for(let i = 1; i <= listLi.length; i++){
+    firstChild.firstElementChild.classList.add("element-" + i);
+    console.log(firstChild.firstElementChild);
+    firstChild=firstChild.nextElementSibling;
+  }
+ 
+  console.log(listLi);
+};
+
+function paresElement(){
+  let selector = document.querySelectorAll("li");
+  let pares = [];
+  let count = 1;
+  for(element of selector){
+    if(count % 2 == 0){
+      pares.push(element);
+    }
+    count ++;
+  }
+
+  let spanSelected = document.querySelector('.selected');
+  let parentUl = spanSelected.parentNode.parentNode;
+
+  pares.forEach(Element => parentUl.removeChild(Element));
+  
+};
+
+function secondList(){
+  let list2 = document.getElementById("list2");
+  var li1 = document.createElement("li");
+  var li2 = document.createElement("li");
+  var li3 = document.createElement("li");
+
+  var button1 = document.createElement("button");
+  var button2 = document.createElement("button");
+  var button3 = document.createElement("button");
+
+  button1.innerHTML = "texto ejemplo 1";
+  button2.innerHTML = "texto ejemplo 3";
+  button3.innerHTML = "texto ejemplo 5";
+
+  button3.setAttribute("disabled", "");
+
+  li1.appendChild(button1);
+  li2.appendChild(button2);
+  li3.appendChild(button3);
+
+  list2.appendChild(li1);
+  list2.appendChild(li2);
+  list2.appendChild(li3);
+   
+  console.log(list2);
+
+}
+
 function onLoad() {
-  console.log("hi");
+  firstList();
+  paresElement();
+  secondList();
+ 
 }
